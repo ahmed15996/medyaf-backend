6b62970bdbe8cdc4e620d631e4ee06fd055cebc3
tarek-rady <tarekrady1998@gmail.com>
2024-07-15 20:47:18 +0300

    dash2

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/opt', function () {
-    Artisan::call('optimize');
-    return 0;
-});
+
 
 Route::get('language/{locale}', function ($locale) {
     app()->setLocale($locale);
